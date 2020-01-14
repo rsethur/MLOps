6de6fe950051b7ccf6d8d5f724ae58fe7314ef91
rsethur <rsethur@gmail.com>
2020-01-14 19:09:40 +0400

    batch inference

--- models/utils/batch_inference/run_batch_inf_pipeline.py
+++ models/utils/batch_inference/run_batch_inf_pipeline.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from azureml.pipeline.core import PublishedPipeline
 from azureml.core import Experiment
 from dotenv import load_dotenv
@@ -8,11 +9,18 @@ def main():
     load_dotenv()
     ws = AzureMLUtils.get_workspace()
     print("got workspace")
-    pipeline_id = "87744497-04b1-4d6c-ad8e-519e84a4ca1c"
-    print("PIPELINE_ID !!!!: ",pipeline_id)
+    pipeline_id = getRuntimeArgs()
+    print("PIPELINE_ID: ", pipeline_id)
     published_pipeline = PublishedPipeline.get(workspace=ws, id=pipeline_id)
+    print("published pipeline: ", published_pipeline)
     pipeline_run = Experiment(ws, 'batch-run-code').submit(published_pipeline)
     pipeline_run.wait_for_completion(show_output=True)
+
+def getRuntimeArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--PIPELINE_ID', type=str)
+    args = parser.parse_args()
+    return args.PIPELINE_ID
 
 if __name__ == "__main__":
     main()
